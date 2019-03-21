35a56020afb7cfd628bf16f8c199996272fab2ac
Jordan Priestman <jordanpriestman@Jordans-MBP.fc.ca>
2019-03-21 17:03:39 -0400

    finished tutorial files

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    user: "root",
+    password: "root", // leave this blank for windows
+    port:8889, // for windows and linex its 3306, 8889 for mac
+    host: "localhost",
+    database: "marvelcharacters",
+
+}
+
+module.exports = config;
